605afd3b46498f6e1563079e4c97d0737ef158f9
Saul Mendoza - Home <saulmendoza08@gmail.com>
2023-11-23 21:00:20 -0300

    hoja 3 temrinada

--- terceraPagina-funciones-colecciones.js
+++ terceraPagina-funciones-colecciones.js
@@ -4,7 +4,11 @@ que toma una serie de temperaturas en grados Celsius y convierte
 la temperatura en grados Celsius dada a Fahrenheit. 
 */
 
-
+const celsiusToFahrenheit = (tempCelsius)=>{
+    const f = (tempCelsius * 1.8) + 32;
+    return f;
+}
+console.log(celsiusToFahrenheit(30));
 
 
 /*2
@@ -13,12 +17,20 @@ esta función a cada temperatura en el array celsiusTemperatures ,
 creando un nuevo array fahrenheitTemperatures con los valores convertidos. 
 */
 
+const celsiusTemperatures = [25, 30, 15, 10, 20];
+const fahrenheitTemperatures = celsiusTemperatures.map(celsiusToFahrenheit);
+
+
+
 /*3
 Finalmente, las matrices de temperatura originales y
 convertidas se imprimirán en la consola. 
 */
 
-/*4
+console.log("Temperaturas en Celsius:", celsiusTemperatures);
+console.log("Temperaturas en Fahrenheit:", fahrenheitTemperatures);
+
+/*
 Asegúrese de utilizar varias funciones en esta práctica, 
 una para convertir de grados Celsius a Fahrenheit y otra para 
 obtener los valores iniciales, llame a la función Celsius 

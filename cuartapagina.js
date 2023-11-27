7450d77a95ad0b295ec7fbc0777c047c11688fbf
Saul Mendoza - Home <saulmendoza08@gmail.com>
2023-11-26 21:48:29 -0300

    agregado modulos y cuarta hoja

---
+++ cuartapagina.js
@@ -0,0 +1,50 @@
+const { default: axios } = require("axios");
+
+/*
+Necesitamos definir una función llamada fetchData que tome una URL de parámetro, que represente la URL desde la cual recuperar datos.
+
+Una vez creada la función fetchData creamos una nueva promesa y dentro de su constructor usamos la función setTimeout para simular un retraso de 2 segundos. 
+ 
+Después del retraso, resolvamos aleatoriamente la promesa con un objeto de datos simulado o la rechacemos con un objeto de error simulado.
+Fuera del constructor Promise, devuelve la promesa creada.
+
+Finalmente, llamemos a la función fetchData con una URL y manejemos los casos resueltos y rechazados usando .then() y .catch() .
+
+La URL y los datos simulados se pueden definir con una URL de API, como https://swapi.dev/ ; 
+opcionalmente, agregue el uso de una biblioteca como Axios o Chai HTTP, 
+para recuperar correctamente los datos a través de servicios API.
+*/
+function fetchData(url) {
+  if (typeof url === "")
+    return Promise.reject(`Error en la url ingresada: ${url}`);
+  promecita = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        url: url,
+        resultado: "La dureccion url es " + url,
+      });
+    }, 2000);
+  });
+
+  return promecita;
+}
+
+/*omito la ejecion para que se mas claro porque voy a crear una promesa con axios
+fetchData("https://swapi.dev/")
+    .then((objeto)=>{
+        console.log("Inicio de la promesa");
+        console.log(`Promesa: ${objeto.url}, ${objeto.resultado}`);
+    })
+    .catch(err => console.error (err));
+*/
+
+//version adicional con axios (librerias extrernas)
+
+axios.get("https://swapi.dev/api/people/1")
+    .then(respuesta => {
+        console.log(respuesta);
+        let json =  respuesta.data;
+        console.log (`El personaje es : ${json.name}`)
+    })
+    .catch()
+    .finally();
